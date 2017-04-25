7967e520de4c43a86ccbb07f3cf1b884aee988ef
Juan Pablo Contreras Franco <juanp.contreras@gmail.com>
2017-04-25 14:51:44 +0200

    Added function dom2path and made it visible. Removed duplicated function.

--- svgpathtools/svg2paths.py
+++ svgpathtools/svg2paths.py
@@ -52,34 +52,6 @@ def ellipse2pathd(ellipse):
 
     return d
 
-
-def ellipse2pathd(ellipse):
-    """converts the parameters from an ellipse or a circle to a string for a Path
-    object d-attribute"""
-
-    cx = ellipse.get('cx', None)
-    cy = ellipse.get('cy', None)
-    rx = ellipse.get('rx', None)
-    ry = ellipse.get('ry', None)
-    r = ellipse.get('r', None)
-
-    if r is not None:
-        rx = ry = float(r)
-    else:
-        rx = float(rx)
-        ry = float(ry)
-
-    cx = float(cx)
-    cy = float(cy)
-
-    d = ''
-    d += 'M' + str(cx - rx) + ',' + str(cy)
-    d += 'a' + str(rx) + ',' + str(ry) + ' 0 1,0 ' + str(2 * rx) + ',0'
-    d += 'a' + str(rx) + ',' + str(ry) + ' 0 1,0 ' + str(-2 * rx) + ',0'
-
-    return d
-
-
 def polygon2pathd(polyline_d):
     """converts the string from a polygon points-attribute to a string for a
     Path object d-attribute.
@@ -131,8 +103,37 @@ def svg2paths(svg_file_location,
     if os_path.dirname(svg_file_location) == '':
         svg_file_location = os_path.join(getcwd(), svg_file_location)
 
-    doc = parse(svg_file_location)
+    return dom2path(parse(svg_file_location),
+                    convert_lines_to_paths,
+                    convert_polylines_to_paths,
+                    convert_polygons_to_paths,
+                    return_svg_attributes,
+                    convert_ellipses_to_paths)
 
+def dom2path(doc,
+             convert_lines_to_paths=True,
+             convert_polylines_to_paths=True,
+             convert_polygons_to_paths=True,
+             return_svg_attributes=False,
+             convert_ellipses_to_paths=True):
+    """
+    Converts a DOM element into a list of Path objects and a list of
+    dictionaries containing their attributes.  This currently supports
+    SVG Path, Line, Polyline, Circle and Ellipse, and Polygon elements.
+    :param svg_file_location: the location of the svg file
+    :param convert_lines_to_paths: Set to False to disclude SVG-Line objects
+    (converted to Paths)
+    :param convert_polylines_to_paths: Set to False to disclude SVG-Polyline
+    objects (converted to Paths)
+    :param convert_polygons_to_paths: Set to False to disclude SVG-Polygon
+    objects (converted to Paths)
+    :param return_svg_attributes: Set to True and a dictionary of
+    svg-attributes will be extracted and returned
+    :param convert_ellipses_to_paths: Set to False to disclude SVG-Ellipse
+    objects (converted to Paths). Circles are treated as ellipses.
+    :return: list of Path objects, list of path attribute dictionaries, and
+    (optionally) a dictionary of svg-attributes
+    """
     def dom2dict(element):
         """Converts DOM elements to dictionaries of attributes."""
         keys = list(element.attributes.keys())
